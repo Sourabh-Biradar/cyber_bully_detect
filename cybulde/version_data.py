c193702c214936f47a6c6ad9ac372faa67613f01
Sourabh Biradar <sourabh@sourabh.local>
2026-03-24 21:32:02 +0530

    Added data verion tags & moved data to cloud

--- cybulde/version_data.py
+++ cybulde/version_data.py
@@ -2,13 +2,14 @@ from pathlib import Path
 
 from cybulde.config_schemas.config_schema import Config
 from cybulde.utils.config_utils import get_config
-from cybulde.utils.data_utils import initilaize_dvc,initialize_dvc_storage
+from cybulde.utils.data_utils import commit_to_dvc,initilaize_dvc,initialize_dvc_storage
 
 
 @get_config(config_path="../configs", config_name="config")
 def version_data(config: Config) -> None:
     initilaize_dvc()
     initialize_dvc_storage(config.dvc_remote_name,config.dvc_remote_url)
+    commit_to_dvc(config.dvc_raw_data_folder,config.dvc_remote_name)
 
     
 if __name__ == "__main__":
